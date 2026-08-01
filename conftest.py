1ba1b2722eff350bd5243425cbefedc3722c3035
Nikolay Zubayrov <zubayrovn@mail.ru>
2026-08-01 17:42:14 +0300

    Обновлён conftest.py

--- conftest.py
+++ conftest.py
@@ -10,9 +10,12 @@ from pages.order_overview import OrderOverview
 def driver():
     options = Options()  # для Докера
     options.add_argument('--headless')  # для Докера
+    options.add_argument('--no-sandbox')  # для Докера
     options.add_argument('--disable-dev-shm-usage')  # для Докера
+    options.add_argument('--disable-gpu')  # для Докера
+    options.add_argument('--window-size=1920,1080')  # ВСТАВЬТЕ ЭТУ СТРОЧКУ
     chrome_driver = webdriver.Chrome(options=options)  # для Докера
-    chrome_driver.maximize_window()
+    # chrome_driver.maximize_window()
     yield chrome_driver
     chrome_driver.quit()
 
